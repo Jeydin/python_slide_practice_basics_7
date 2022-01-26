d58432e40c91fe25e447d5d8269bfd2f12f1e23b
Jeydin Pham <89167700+Jeydin@users.noreply.github.com>
2022-01-26 17:35:28 +0000

    Finished Day 1

---
+++ main.py
@@ -0,0 +1,4 @@
+# Jeydin Pham
+# Period 4
+# January 26, 2022
+# Title - Algorithm Notes
--- slide_practice.py
+++ slide_practice.py
@@ -1,3 +1,54 @@
 '''
 Algorithm: Process (step-by-step) where a problem can be solved that will always give the right answer
+
+A series of steps that solves a problem
+v = 3 * 2
+print(v)
+
+1. Alarm goes off
+2. Snooze alarm
+3. Hopefully wake up later
+
+Three main categories of algorithms
+"The building blocks" of algorithms
+
+Sequencing - List of steps (goes in order)
+Selection - Ifs/elses (choices)
+Iteration - Loops (repeating something)
+
+
+Building Block #1: Sequencing
+Q: What is 2 to the 5th power?
+start = 2 * 2
+nxt = start * 2
+nxt2 = nxt * 2
+ans = next2 * 2
+
+Real World:
+1. Alarm goes off
+2. Smash the snoooze button
+3. Back to sleep
+
+Building Block #2: Iteration
+1st = [2, 2, 2, 2]
+ans = 2
+for x in list:
+	ans = and * x
+
+1. For each homework problem
+	- Solve problem
+
+Building Block #3: Selection
+if ans < 32:
+	ans *= 2
+else:
+	return ans
+
+If it is weekend, sleep in
+Else, wake up early
+
+Three types of sorting algorithms:
+1. Bubble sort
+2. Selection source
+3. Insertion sort
 '''
